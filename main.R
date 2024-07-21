37f8455e2ebf529e9ec16e601d4d8043e60114f9
hugosanta-hub <81754055+hugosanta-hub@users.noreply.github.com>
2024-07-21 16:58:02 +0200

    longer name

--- main.R
+++ main.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(here)
 
-tv_hours_tab <- gss_cat %>% 
+tv_hours_tabulation <- gss_cat %>% 
   filter (age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours= mean(tvhours, na.rm= T))
 
-write_csv(tv_hours_tab, here("tv_hours_table.csv"))
+write_csv(tv_hours_tabulation, here("tv_hours_table.csv"))
